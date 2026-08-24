d93c0215abc4bbf1d2c93c160a00027f00ce5a1c
mahakagarwal7 <mahakagrwl07@gmail.com>
2026-08-24 10:48:39 +0530

    backend files

---
+++ backend/index.js
@@ -0,0 +1,18 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from 'dotenv';
+
+dotenv.config()
+
+const port = 8081
+
+mongoose.connect(process.env.dbURL).then(() =>{
+    console.log("DB Connected");
+}).catch((err) =>{
+    console.log(err);
+})
+const app = express();
+
+app.listen(port,() => {
+    console.log(`Server Started at ${port}`);
+})
